732ec4fef138fb88268f97a7fdd37d162ce9ced2
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-03-04 08:27:37 +0300

    04.030.2021 Work

--- app/src/main/java/com/example/fiftheen/GameScreenActivity.kt
+++ app/src/main/java/com/example/fiftheen/GameScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fiftheen
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game_screen.*
 
@@ -10,24 +11,49 @@ class GameScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
         tvTimer.start()
-        val numbers = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        val numbers = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
         numbers.shuffle()
-        line1Button1.text = numbers[0].toString()
-        line1Button2.text = numbers[1].toString()
-        line1Button3.text = numbers[2].toString()
-        line1Button4.text = numbers[3].toString()
-        line2Button1.text = numbers[4].toString()
-        line2Button2.text = numbers[5].toString()
-        line2Button3.text = numbers[6].toString()
-        line2Button4.text = numbers[7].toString()
-        line3Button1.text = numbers[8].toString()
-        line3Button2.text = numbers[9].toString()
-        line3Button3.text = numbers[10].toString()
-        line3Button4.text = numbers[11].toString()
-        line4Button1.text = numbers[12].toString()
-        line4Button2.text = numbers[13].toString()
-        line4Button3.text = numbers[14].toString()
-        line4Button4.text = numbers[15].toString()
+        val buttons = mutableListOf<Button>(
+            line1Button1,
+            line1Button2,
+            line1Button3,
+            line1Button4,
+            line2Button1,
+            line2Button2,
+            line2Button3,
+            line2Button4,
+            line3Button1,
+            line3Button2,
+            line3Button3,
+            line3Button4,
+            line4Button1,
+            line4Button2,
+            line4Button3,
+            line4Button4
+        )
+        buttons.shuffle()
+        for (i in 0..14) {
+            buttons[i].text = numbers[i].toString()
+        }
+
+
+//
+//        line1Button1.text = numbers[0].toString()
+//        line1Button2.text = numbers[1].toString()
+//        line1Button3.text = numbers[2].toString()
+//        line1Button4.text = numbers[3].toString()
+//        line2Button1.text = numbers[4].toString()
+//        line2Button2.text = numbers[5].toString()
+//        line2Button3.text = numbers[6].toString()
+//        line2Button4.text = numbers[7].toString()
+//        line3Button1.text = numbers[8].toString()
+//        line3Button2.text = numbers[9].toString()
+//        line3Button3.text = numbers[10].toString()
+//        line3Button4.text = numbers[11].toString()
+//        line4Button1.text = numbers[12].toString()
+//        line4Button2.text = numbers[13].toString()
+//        line4Button3.text = numbers[14].toString()
+//        line4Button4.text = numbers[15].toString()
     }
 }
 
