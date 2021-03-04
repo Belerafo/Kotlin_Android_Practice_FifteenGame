f1ce4f05247e6f1ae5d3abb0c5a5f56bb545ee36
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-03-04 11:43:14 +0300

    04.030.2021 Work

--- app/src/main/java/com/example/fiftheen/GameScreenActivity.kt
+++ app/src/main/java/com/example/fiftheen/GameScreenActivity.kt
@@ -1,18 +1,24 @@
 package com.example.fiftheen
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game_screen.*
 
-@Suppress("DEPRECATED_IDENTITY_EQUALS")
+
 class GameScreenActivity : AppCompatActivity() {
+    private var buttonsopen = mutableListOf<Button>()
+    private val numbersopen = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
         tvTimer.start()
-        val numbers = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-        numbers.shuffle()
+        tvTimer.setOnChronometerTickListener {
+            onFinishListener()
+        }
+        val numbers = numbersopen
+        //numbers.shuffle()
         val buttons = mutableListOf<Button>(
             line1Button1,
             line1Button2,
@@ -31,30 +37,45 @@ class GameScreenActivity : AppCompatActivity() {
             line4Button3,
             line4Button4
         )
+        buttonsopen = buttons
+
         buttons.shuffle()
         for (i in 0..14) {
             buttons[i].text = numbers[i].toString()
         }
 
-
-//
-//        line1Button1.text = numbers[0].toString()
-//        line1Button2.text = numbers[1].toString()
-//        line1Button3.text = numbers[2].toString()
-//        line1Button4.text = numbers[3].toString()
-//        line2Button1.text = numbers[4].toString()
-//        line2Button2.text = numbers[5].toString()
-//        line2Button3.text = numbers[6].toString()
-//        line2Button4.text = numbers[7].toString()
-//        line3Button1.text = numbers[8].toString()
-//        line3Button2.text = numbers[9].toString()
-//        line3Button3.text = numbers[10].toString()
-//        line3Button4.text = numbers[11].toString()
-//        line4Button1.text = numbers[12].toString()
-//        line4Button2.text = numbers[13].toString()
-//        line4Button3.text = numbers[14].toString()
-//        line4Button4.text = numbers[15].toString()
     }
+
+    // Проверка на конец игры
+    fun onFinishListener() {
+        var finish = 0
+        for (i in 0..14) {
+            if (buttonsopen[i].text == (i + 1).toString()) {
+                finish++
+                //testView.text =" ${testView.text} ${buttonsopen[i].text} + кнопка $i"
+            }
+        }
+        if (finish == 15) {
+            //testView.text = finish.toString()
+            tvTimer.stop()
+        } else {
+            //testView.text = finish.toString()
+            finish = 0
+        }
+    }
+
+        fun testCkick (view: View){
+
+            for (i in 0..15){
+               buttonsopen[i].setOnClickListener(){
+
+                   testView.text = "${testView.text} ${buttonsopen[i].text} + кнопка $i"
+               }
+            }
+
+        }
+
+
 }
 
 
