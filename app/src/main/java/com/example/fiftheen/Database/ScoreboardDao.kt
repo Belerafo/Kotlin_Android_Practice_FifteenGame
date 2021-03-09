e5294f9a20cf7017dd439b06bb88308c6af586c3
Lyshk <lyshka9@yandex.ru>
2021-03-09 22:03:02 +0300

    09.03.2021 home

--- app/src/main/java/com/example/fiftheen/Database/ScoreboardDao.kt
+++ app/src/main/java/com/example/fiftheen/Database/ScoreboardDao.kt
@@ -1,12 +1,13 @@
 package com.example.fiftheen.Database
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 
 @Dao
 interface ScoreboardDao {
-    @get:Query("SELECT * FROM Scoreboard")
-    val all: List<Any?>?
+    @Query("SELECT * FROM Scoreboard")
+    fun getAll(): LiveData<List<Scoreboard>>
 
     @Insert
     fun insert(scoreboard: Scoreboard)
--- app/src/main/java/com/example/fiftheen/MenuActivity.kt
+++ app/src/main/java/com/example/fiftheen/MenuActivity.kt
@@ -4,21 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.room.Database
-
+import com.example.fiftheen.Database.AppDatabase
+import com.example.fiftheen.Database.Scoreboard
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
+
+    private val db = AppDatabase.getInstance(application)
+    val score = db.getScoreboardDao().getAll()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+    }
+
+
 
     fun onStart(view: View) {
         startActivity(Intent(this@MenuActivity, GameScreenActivity::class.java))
+        val employee = Scoreboard()
+        employee.id = 1
+        employee.name = etNameOfGamer.text.toString()
+        employee.timeOfEnd = 10000
+        db.getScoreboardDao().insert(employee)
     }
 
     fun onExit(view: View) {
         finish()
     }
-}}
+}
 
